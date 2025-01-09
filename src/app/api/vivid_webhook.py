eda9ef742b91202277ee194be5f8e72412c56ad1
Hasan Rasulov <hrasulov@ticket.com>
2025-01-09 18:06:40 +0500

    fix insert query

--- src/app/api/vivid_webhook.py
+++ src/app/api/vivid_webhook.py
@@ -265,9 +265,9 @@ def _upload_into_postgres(sale, vivid_account):
             insert_query = """
                     INSERT INTO vivid_sales (
                         id, order_id, broker_ticket_id, vivid_account_id, section, "row", notes, quantity, cost, event,
-                        event_date, order_date, venue, status, electronic_delivery,  bar_codes_required,
+                        event_date, order_date, venue, status, electronic_delivery, bar_codes_required,
                         instant_flash_seats, collection_session_ts, event_url
-                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                     ON CONFLICT (order_id)
                     DO NOTHING
                 """
